f7ec398258260837c967408516608f5534473884
George Redinger <george@georgeredinger.com>
2014-02-13 18:42:29 -0800

    checkpoint

--- Glove/Glove.ino
+++ Glove/Glove.ino
@@ -6,12 +6,13 @@ const int LED = 13;
 long count=0;
 
 typedef  struct {
+  unsigned index;
   unsigned long mills;
   sensors_event_t event;
 } 
 cap;
-
-CircularBuffer<cap,1000> capture;
+#define BUFFERSIZE 250
+CircularBuffer<cap,BUFFERSIZE> capture;
 
 // This example code is in the public domain.
 
@@ -20,116 +21,6 @@ Adafruit_ADXL345_Unified accel = Adafruit_ADXL345_Unified(12345);
 
 boolean fast=false;
 
-void displaySensorDetails(void)
-{
-  sensor_t sensor;
-  accel.getSensor(&sensor);
-  Serial.println("------------------------------------");
-  Serial.print  ("Sensor:       "); 
-  Serial.println(sensor.name);
-  Serial.print  ("Driver Ver:   "); 
-  Serial.println(sensor.version);
-  Serial.print  ("Unique ID:    "); 
-  Serial.println(sensor.sensor_id);
-  Serial.print  ("Max Value:    "); 
-  Serial.print(sensor.max_value); 
-  Serial.println(" m/s^2");
-  Serial.print  ("Min Value:    "); 
-  Serial.print(sensor.min_value); 
-  Serial.println(" m/s^2");
-  Serial.print  ("Resolution:   "); 
-  Serial.print(sensor.resolution); 
-  Serial.println(" m/s^2");  
-  Serial.println("------------------------------------");
-  Serial.println("");
-  delay(500);
-}
-
-void displayDataRate(void)
-{
-  Serial.print  ("Data Rate:    "); 
-
-  switch(accel.getDataRate())
-  {
-  case ADXL345_DATARATE_3200_HZ:
-    Serial.print  ("3200 "); 
-    break;
-  case ADXL345_DATARATE_1600_HZ:
-    Serial.print  ("1600 "); 
-    break;
-  case ADXL345_DATARATE_800_HZ:
-    Serial.print  ("800 "); 
-    break;
-  case ADXL345_DATARATE_400_HZ:
-    Serial.print  ("400 "); 
-    break;
-  case ADXL345_DATARATE_200_HZ:
-    Serial.print  ("200 "); 
-    break;
-  case ADXL345_DATARATE_100_HZ:
-    Serial.print  ("100 "); 
-    break;
-  case ADXL345_DATARATE_50_HZ:
-    Serial.print  ("50 "); 
-    break;
-  case ADXL345_DATARATE_25_HZ:
-    Serial.print  ("25 "); 
-    break;
-  case ADXL345_DATARATE_12_5_HZ:
-    Serial.print  ("12.5 "); 
-    break;
-  case ADXL345_DATARATE_6_25HZ:
-    Serial.print  ("6.25 "); 
-    break;
-  case ADXL345_DATARATE_3_13_HZ:
-    Serial.print  ("3.13 "); 
-    break;
-  case ADXL345_DATARATE_1_56_HZ:
-    Serial.print  ("1.56 "); 
-    break;
-  case ADXL345_DATARATE_0_78_HZ:
-    Serial.print  ("0.78 "); 
-    break;
-  case ADXL345_DATARATE_0_39_HZ:
-    Serial.print  ("0.39 "); 
-    break;
-  case ADXL345_DATARATE_0_20_HZ:
-    Serial.print  ("0.20 "); 
-    break;
-  case ADXL345_DATARATE_0_10_HZ:
-    Serial.print  ("0.10 "); 
-    break;
-  default:
-    Serial.print  ("???? "); 
-    break;
-  }  
-  Serial.println(" Hz");  
-}
-
-void displayRange(void)
-{
-  Serial.print  ("Range:         +/- "); 
-
-  switch(accel.getRange())
-  {
-  case ADXL345_RANGE_16_G:
-    Serial.print  ("16 "); 
-    break;
-  case ADXL345_RANGE_8_G:
-    Serial.print  ("8 "); 
-    break;
-  case ADXL345_RANGE_4_G:
-    Serial.print  ("4 "); 
-    break;
-  case ADXL345_RANGE_2_G:
-    Serial.print  ("2 "); 
-    break;
-  default:
-    Serial.print  ("?? "); 
-    break;
-  }  
-  Serial.println(" g");  
-}
 
 void setup()
 {
@@ -139,7 +30,11 @@ void setup()
   Serial1.println("LED OFF. Press 1 to LED ON!");  // print message
   Serial1.println("LED ON. Press 0 to LED OFF!");
 
-
+  char s[20];
+  Serial.println("before sprintf");
+  sprintf(s,"%f\n","123.456");
+  Serial.print(s);
+  Serial.println("after sprintf");
   if(!accel.begin())
   {
     /* There was a problem detecting the ADXL345 ... check your connections */
@@ -152,14 +47,6 @@ void setup()
   // displaySetRange(ADXL345_RANGE_8_G);
   // displaySetRange(ADXL345_RANGE_4_G);
   // displaySetRange(ADXL345_RANGE_2_G);
-
-  /* Display some basic information on this sensor */
-  displaySensorDetails();
-
-  /* Display additional settings (outside the scope of sensor_t) */
-  displayDataRate();
-  displayRange();
-  Serial.println("");
 
 }
 
@@ -184,37 +71,53 @@ void loop() {
   }
 
   if(fast){
-    long start=millis();
+    fast=false;
+    unsigned long trigger_time;
+    unsigned long start=millis();
+    unsigned index=0;
     capture.clear();
     sensors_event_t event; 
     cap capt;
-    for(int i=0;i<5000;i++){
+    bool trigger=false;
+    int sample=0;
+    while((millis()-start)<10000){
       accel.getEvent(&event);
+      capt.index=index++;
       capt.mills=millis();
       capt.event = event;
       capture.push(capt);
-      /* Display the results (acceleration is measured in m/s^2) */
-      //Serial.print(millis());Serial.print(",");
-      //Serial.print(event.acceleration.x); Serial.print(",");
-      //Serial.print(event.acceleration.y); Serial.print(",");
-      //Serial.println(event.acceleration.z); 
+       if(trigger){
+         if(sample++ > BUFFERSIZE/4)
+          break;
+      } else  if((abs(event.acceleration.x) + abs(event.acceleration.y) + abs(event.acceleration.z)) > 40.0) {
+        trigger=true;
+        trigger_time=millis();
+      }
+     
+      delay(3);
     }
-    Serial.println(millis()-start);
-    while(capture.remain()>0){
-      capt=capture.pop();
-      Serial.print(capt.mills);
-      Serial.print(",");
-      Serial.print(capt.event.acceleration.x); 
-      Serial.print(",");
-      Serial.print(capt.event.acceleration.y); 
-      Serial.print(",");
-      Serial.println(capt.event.acceleration.z);
-    }
-    digitalWrite(LED, LOW);  // if 1, switch LED Off
 
-    fast=false;
+    digitalWrite(LED, LOW);  // if 1, switch LED Off
+    if(trigger){
+       unsigned long end = capture.pop().mills;
+
+      Serial1.println("Start");
+      while(capture.remain()> 0){
+        capt=capture.pop();
+        char s[30];
+        sprintf(s,"%d,%2.1f,%2.1f,%2.1f\n",(capt.index),capt.event.acceleration.x,capt.event.acceleration.y,capt.event.acceleration.z);
+        Serial.print(s);
+        Serial1.print(s);
+      }
+      Serial1.println("End");
+    }
+    else{
+      Serial1.println("no trigger");
+    }
   }
 }
+
+
 
 
 
